d965d011f3453b996805c7e3b453a6f5aa6602ce
Nathan Rossow <nathanrossow@gmail.com>
2026-07-10 15:49:58 -0500

    Update vcstopdf-v1.3.2.py

--- vcstopdf-v1.3.2.py
+++ vcstopdf-v1.3.2.py
@@ -436,6 +436,12 @@ scikit-learn==1.5.0"""
 streamlit-option-menu==0.3.12
 numpy==2.1.2
 pandas==2.2.3"""
+    },
+    "Dependency Details (Python 3.13.14)": {
+        "project_type": "Dependency Details",
+        "python_version": "Python 3.13.14",
+        "requirements": """streamlit==1.39.0
+pandas==2.2.3"""
     }
     # Add more presets here in the future
 }
